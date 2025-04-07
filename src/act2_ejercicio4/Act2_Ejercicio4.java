5d9b6099e8a8fbc687604e1c658c2b14cbdb9e04
PauMOSA <darcksharck9@outlook.com>
2025-04-07 12:06:18 -0600

    Upload code

---
+++ src/act2_ejercicio4/Act2_Ejercicio4.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package act2_ejercicio4;
+/**
+ *
+ * @GLORIA PAULINA MORENO SALDIVAR
+ */
+public class Act2_Ejercicio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        for(int i = 0; i < 100; i++)
+        {
+            if(i % 3 == 0 && i % 5 == 0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
